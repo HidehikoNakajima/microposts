1a7cf82f4fbedada349d9a5fbf412bf961ec99f5
hidehikonakajima <hide_28soba@bk2.so-net.ne.jp>
2016-01-10 07:19:42 +0000

    1st submission 1.02

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
   
+  before_action :set_user, only: [:edit, :update]
+  before_action :correct_user, only: [:show, :edit, :update]
+  
   def show
     @user = User.find(params[:id])
   end  
@@ -18,10 +21,34 @@ class UsersController < ApplicationController
     end
   end
   
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    if @user.update(user_params)
+      redirect_to root_path , notice: 'ユーザ情報を更新しました'
+    else
+      render 'edit'
+    end
+  end
+
   private
   
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+  
+  def set_user
+    @user = User.find(params[:id])
+  end
+  
+  def correct_user
+    @user = User.find(params[:id])
+    unless @user == current_user
+      flash[:danger] = '不正なアクセスです'
+      redirect_to root_path
+    end
   end
   
 end
