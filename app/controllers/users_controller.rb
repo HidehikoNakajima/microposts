763a0f6e573522d2bf8fead9ddb0e3b260e267c6
hidehikonakajima <hide_28soba@bk2.so-net.ne.jp>
2016-01-12 12:02:23 +0000

    1st submission ver.1.12

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   
   before_action :set_user, only: [:edit, :update]
-  before_action :correct_user, only: [:show, :edit, :update]
+  before_action :correct_user, only: [:edit, :update]
   
   def show
     @user = User.find(params[:id])
