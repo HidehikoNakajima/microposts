1fd0e28124343c69c55268522b510822a4ecb25a
hidehikonakajima <hide_28soba@bk2.so-net.ne.jp>
2016-01-12 12:23:29 +0000

    1st submission ver.1.13

--- db/migrate/20160112111341_create_microposts.rb
+++ db/migrate/20160112111341_create_microposts.rb
@@ -1,11 +1,11 @@
 class CreateMicroposts < ActiveRecord::Migration
   def change
     create_table :microposts do |t|
-      t.reference :user, index: true, foreign_key: true
+      t.references :user, index: true, foreign_key: true
       t.text :content
 
       t.timestamps null: false
-      t.index[:user_id, :created_at]
+      t.index [:user_id, :created_at]
     end
   end
 end
